bac22b220ca9b1befb9a1cac71c677439a8e89fa
erlinolaya727 <67381481+erlinolaya727@users.noreply.github.com>
2022-07-21 20:36:00 -0500

    Se ajusta configuracion del  archivo app y se cargan archivos de bases de datos

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from env import app
+from flask import Flask, render_template
 
 if __name__ == '__main__':
     app.run(debug = True)
+    
+@app.route('/')
+def home_page():
+    return render_template('index.html')
