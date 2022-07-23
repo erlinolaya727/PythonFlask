276b46034df4b1076a68613ac6608eed0d201c01
Vicente Franco <viso16@gmail.com>
2022-07-23 11:29:26 -0500

    base_gestor_admin_flask

--- env/__init__.py
+++ env/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path="/static")
 
 @app.route('/')
+@app.route('/home')
 def home_page():
-    return render_template('index.html')
+    return render_template('panelSuper.html')
