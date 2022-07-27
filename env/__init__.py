6263bf329337f0147eb9ef8360b0deb58deaec08
erlinolaya727 <67381481+erlinolaya727@users.noreply.github.com>
2022-07-27 02:06:13 -0500

    Se crea plantilla con extends e include

--- env/__init__.py
+++ env/__init__.py
@@ -4,4 +4,4 @@ app = Flask(__name__,static_url_path="/static")
 
 @app.route('/')
 def home_page():
-    return render_template('templateHotel.html')
+    return render_template('administrador/index.html')
