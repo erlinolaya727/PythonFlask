0d5793e01a9c5bdd79f49c3a57e2c71be1222fcc
erlinolaya727 <erlinolaya727@gmail.com>
2022-08-12 01:31:58 -0500

    Jorge-Vicente-Sebastian-Erlin

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 from env import app
-from flask import Flask, render_template
 
 if __name__ == '__main__':
     app.run(debug = True)
-    
-@app.route('/')
-def home_page():
-    return render_template('templateHotel.html')
